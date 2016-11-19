2752801acc0d056876ea852225317169306aba29
Eddie <eddie.jostell@gmail.com>
2016-11-19 14:06:47 +0100

    Yet another navbar, this one is pure html/css

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@
     }
 }*/
 
+     /* <ul class="topnav" id="myTopnav">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+                <li>
+                    <a href="#">Projects</a>
+							<ul>
+								<li><a href="#">Todo-app</a></li>
+								<li><a href="#">Food-app</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><a href="#">5</a></li>
+							</ul>
+                </li>       
+                <li class="icon">
+                    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
+                </li>
+            </ul>*/
