c2774140be05702e85e8ef75314ef656c8d3c5a0
Eddie <eddie.jostell@gmail.com>
2016-12-27 01:45:36 +0100

    sorted new links to projects

--- js/script.js
+++ js/script.js
@@ -1,28 +1,9 @@
-/*function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}*/
 
-     /* <ul class="topnav" id="myTopnav">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
-                <li>
-                    <a href="#">Projects</a>
-							<ul>
-								<li><a href="#">Todo-app</a></li>
-								<li><a href="#">Food-app</a></li>
-								<li><a href="#">3</a></li>
-								<li><a href="#">4</a></li>
-								<li><a href="#">5</a></li>
-							</ul>
-                </li>       
-                <li class="icon">
-                    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
-                </li>
-            </ul>*/
+
+$('.project-name').on('click', function () {
+  var url = $(this).data('url');
+  if (url !== undefined) {
+    /*window.location.href = url;*/
+    window.open(url, "_blank");
+  }
+});
